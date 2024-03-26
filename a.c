41c69eb52e7314119a35dc526fb0a55714286525
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-03-25 22:04:22 -0600

    Add files via upload

--- a.c
+++ a.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
+#define PI 3.1416
 
-int main() {
-    int contador = 0;
-    
-    // Ejemplo de while
-    printf("Ejemplo de while:\n");
-    while (contador < 5) {
-        printf("El contador es: %d\n", contador);
-        contador++;
-    }
-    
-    // Reiniciamos el contador para el siguiente ejemplo
-    contador = 0;
-    
-    // Ejemplo de do-while
-    printf("\nEjemplo de do-while:\n");
-    do {
-        printf("El contador es: %d\n", contador);
-        contador++;
-    } while (contador < 5);
-    
+int main (){
+    printf("Pi vale %f" , PI);
     return 0;
 }
